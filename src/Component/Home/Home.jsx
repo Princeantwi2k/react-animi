073ebf1715673384a200cc6bd9396c3b10453f71
princeantwi2k <antwi2k@gmail.com>
2022-01-17 00:28:09 +0800

    added Home

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Dashboad from "./Dashboad";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Dashboad />
+      </div>
+    </>
+  );
+};
+
+export default Home;
