4abc54a1100dafba42affe57f1509a736f7006a1
princeantwi2k <antwi2k@gmail.com>
2022-01-17 00:29:44 +0800

    added as initials

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Store from './Component/Store'
-import {Provider} from "react-redux"
+
+
+
+
+
+
+
 
 ReactDOM.render(
- 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
 
+  <React.StrictMode> 
+       <App /> 
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
